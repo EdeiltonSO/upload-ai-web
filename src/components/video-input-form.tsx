fce740e44a71589eca1ed52e165abda1fbf34c74
Edeilton Oliveira <edeiltonso@gmail.com>
2023-09-21 23:42:53 -0300

    feat: add axios e define constantes de status

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -6,9 +6,21 @@ import { Button } from "./ui/button";
 import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react";
 import { getFFmpeg } from "@/lib/ffmpeg";
 import { fetchFile } from "@ffmpeg/util";
+import { api } from "@/lib/axios";
+
+type Status = 'waiting' | 'converting' | 'uploading' | 'generating' | 'success'
+
+const statusMessages = {
+  converting: 'Convertendo...',
+  generating: 'Transcrevendo...',
+  uploading: 'Carregando...',
+  success: 'Sucesso!'
+}
 
 export function VideoInputForm() {
     const [videoFile, setVideoFile] = useState<File | null>(null)
+    const [status, setStatus] = useState<Status>('waiting')
+    
     const promptInputRef = useRef<HTMLTextAreaElement>(null)
 
     function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
@@ -60,15 +72,28 @@ export function VideoInputForm() {
     async function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
       event.preventDefault()
 
-      // const prompt = promptInputRef.current?.value
+      const prompt = promptInputRef.current?.value
+      if (!videoFile) { return; }
 
-      if (!videoFile) {
-        return;
-      }
+      setStatus('converting')
 
       const audioFile = await convertVideoToAudio(videoFile)
+      const data = new FormData()
+      data.append('file', audioFile)
 
-      console.log(audioFile)
+      setStatus('uploading')
+
+      const response = await api.post('/videos', data)
+      const videoId = response.data.video.id
+
+      setStatus('generating')
+
+      console.log(videoId)
+      await api.post(`/videos/${videoId}/transcription`, {
+        prompt
+      })
+
+      setStatus('success')
     }
 
     const previewURL = useMemo(() => {
@@ -101,15 +126,27 @@ export function VideoInputForm() {
           <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
           <Textarea
             ref={promptInputRef}
+            disabled={status !== 'waiting'}
             id="transcription_prompt"
             className="min-h-20 leading-relaxed"
             placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula"
           />
         </div>
 
-        <Button type="submit" className="w-full">
-          Carregar vídeo
-          <Upload className="w-4 h-4 ml-2" />
+        <Button 
+          data-success={status === 'success'}
+          disabled={status !== 'waiting'} 
+          type="submit" 
+          className="w-full data-[success=true]:bg-emerald-400"
+        >
+          {status === 'waiting' ? (
+            <>
+              Carregar vídeo
+              <Upload className="w-4 h-4 ml-2" />
+            </>
+          ) : (
+            statusMessages[status]
+          )}
         </Button>
       </form>
     )
